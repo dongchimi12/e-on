702478654a20f7d51688f4f0e5bf6b3c6845f326
dongchimi12 <chon1520@kyonggi.ac.kr>
2021-03-24 21:10:41 +0900

    e-on 재 업로드

---
+++ e-on re.py
@@ -0,0 +1,24 @@
+on = int(input("수를 입력하세요 : "))
+b = []
+c = 0
+
+while on != 0:
+    b.append(on)
+    on = int(input("수를 입력하세요 : "))
+    c += 1
+
+
+def angry(a,t):
+    d = 0
+    t = 0
+    if on == 0 :
+        while d != c :
+          t += a[d]
+          d += 1
+    return t
+
+print(b)
+print("위의 수의 합은", angry(b,0),"입니다")
+
+
+    
