183a38f5c83c262138150b49814801855153bc9c
dongchimi12 <chon1520@kyonggi.ac.kr>
2021-05-03 21:53:44 +0900

    e-on

--- new2.py
+++ new2.py
@@ -5,8 +5,8 @@ def cool(on):
      b = list(map(int,input("자를 시작, 끝, 자른 수의 번호 ").split(",")))
      on = on[b[0]-1 : b[1]-1]
      on.sort()
-     c = on[b[2]-1]
-     return c
+     on = on[b[2]-1]
+     return on
 
 print("자른 수 중에서 k번쨰 수는 ", cool(a) ,"입니다.") 
     
