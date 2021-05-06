0c5c824fdf25fc0d2a0d0663877f3e6d97925912
dongchimi12 <chon1520@kyonggi.ac.kr>
2021-05-06 17:18:41 +0900

    e-on (e)

--- new2.py
+++ new2.py
@@ -3,8 +3,9 @@ a = list(map(int,input("배열 입력 : ").split(",")))
 
 def cool(on):
      b = list(map(int,input("자를 시작, 끝, 자른 수의 번호 ").split(",")))
-     on = on[b[0]-1 : b[1]-1]
+     on = on[b[0]-1 : b[1]]
      on.sort()
+     print(on)
      on = on[b[2]-1]
      return on
 
