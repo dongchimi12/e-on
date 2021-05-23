413ad1ffbd805f338c4a67d90836c5247ea30ed4
dongchimi12 <80456092+dongchimi12@users.noreply.github.com>
2021-05-23 23:51:03 +0900

    Add files via upload

---
+++ bookcenter.py
@@ -0,0 +1,32 @@
+from bookmain import *
+
+print("1 : 도서추가하겠는가?")
+print("2 : 검색해보겠는가?")
+print("3 : 개별적으로 검색하겠는가?")
+print("4 : 도서 정보 수정해볼래?")
+print("5 : 도서 삭제 해볼래?")
+print("6 : 현재 도서 목록을 보자")
+
+print("-----------------------------------------------------------")
+
+def result():
+    a = int(input("자 어떤걸 할까? : "))
+    if a == 1:
+        add()
+    elif a == 2:
+        search()
+    elif a == 3:
+        prisearch()
+    elif a == 4:
+        change()
+    elif a == 5:
+        delete()
+    elif a == 6:
+        hell_yeah()
+    else:
+        print("멈춰 !!!!!!!!!!!!!!!!!!!!!!!!!")
+
+result()
+
+        
+
